530f6e6c9b43ef7da76ded743c4c9bf2d24128e9
ixnp <enprocopios@gmail.com>
2022-04-08 09:03:53 -0700

    starter dom

---
+++ 05-DOM/src/index.js
@@ -0,0 +1,20 @@
+//How does the DOM load our script file?
+
+
+//Selecting Elements
+
+    //Selecting and updating elements
+    //Render Header
+
+    //Creating and appending elements
+    //Render BookList
+
+    //Renders Footer
+   
+    //Removing elements
+    
+
+    
+
+// function calls
+   
