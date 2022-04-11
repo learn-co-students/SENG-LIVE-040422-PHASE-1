4b3704f24700d33158bdfac9544eabd278858bfe
ixnp <enprocopios@gmail.com>
2022-04-11 14:54:08 -0700

    events solution

--- 06-events/src/index.js
+++ 06-events/src/index.js
@@ -1,17 +1,23 @@
+
 //1. Create a delete button element for each card
 //2. Create a form that submits a new book
     //Render Header
     const renderHeader = () => document.querySelector('header h1').textContent = bookStore.name
 
-    
 
     //Render BookList
     const renderBookList = (inventory) => {
         const bookList = document.querySelector('#book-list')
-        inventory.forEach((book) => bookList.append(renderBook(book)))
+        inventory.forEach((book) =>{
+            const li = renderBook(book)
+            bookList.append(li)
+        })
     } 
     
     //render one book
+    //Build me a button [x]
+    //DELETE 
+    //Attached to my li
     const renderBook = (book) => {
         const li = document.createElement('li')
         const h3Title = document.createElement('h3')
@@ -19,16 +25,24 @@
         const pPrice = document.createElement('p')
         const pInventory = document.createElement('p')
         const img = document.createElement('img')
-
+        const btn = document.createElement('button')
+       
         h3Title.textContent = book.title
         pAuthor.textContent = book.author 
         pPrice.textContent = `$${book.price}`
         pInventory.textContent = `Inventory: ${book.inventory}` 
-
+        btn.textContent = 'DELETE'
+       
         img.src = book.imageUrl
         li.className = 'list-li'
+
+        //adding events
+        btn.addEventListener('click',(e) => {
+            console.log(e)
+            li.remove()
+        })
      
-        li.append(h3Title, pAuthor, pPrice, pInventory, img)
+        li.append(h3Title, pAuthor, pPrice, pInventory, img, btn)
         return li
     }
 
@@ -43,13 +57,36 @@
         <p> Easley's technical books .inc copyright 2022</p>
         `
     }
+
+    const handleForm = () => {
+        const form = document.querySelector('form')
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            console.log(e.target['form-title'].value)
+            const book ={
+            title: e.target.title.value,
+            author: e.target.author.value,
+            price: e.target.price.value,
+            imageUrl: e.target.imageUrl.value,
+            inventory: e.target.inventory.value,
+            reviews:[]
+            }
+            document.querySelector('#book-list').append(renderBook(book))
+           
+        })
+
+
+    }
  
     
 
 
 // function calls
-renderHeader()
-renderBookList(bookStore.inventory)
-document.querySelector('#book-list li').remove()
-renderFooter()
-handleForm()
+    renderHeader()
+    renderBookList(bookStore.inventory)
+    document.querySelector('#book-list li').remove()
+    renderFooter()
+    handleForm()
+
+
+    
