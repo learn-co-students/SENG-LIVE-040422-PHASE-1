b563ad1cc6508da83a620eaac030ca79ed760e46
ixnp <enprocopios@gmail.com>
2022-04-13 08:57:59 -0700

    starter

---
+++ 08-debugging-in-js/src/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',()=> {
+
+})
+
+// Create an app that list spells from Dungeon and Dragons 5th edition.
+// [] Create a list of name spells from the dnd5eAPI. The endpoint needed is: 'https://www.dnd5eapi.co/api/spells'
+// - every other spell should have the class name of even, and the rest the class name of odd (for CSS reasons) 
+// [] On click display the spells information in the main element. The endpoint needed is: 'https://www.dnd5eapi.co/api/spells/spell_name_here'
+// - Clicking a new spell should remove the old spell. 
+// - name, school, casting time, range components duration, classes, and description should be displayed. 
+// - The main container gains a class name of the card (for CSS reasons)
